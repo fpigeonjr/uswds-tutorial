9ccd23f8718ee626a124165f09707e7312ba4fa0
Frank Pigeon <frank.pigeonjr@gmail.com>
2022-03-11 17:48:32 -0600

    adds uswds overrides

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-const uswds = require('@uswds/compile')
-uswds.paths.dist.theme = './_theme'
+const uswds = require("@uswds/compile")
+uswds.paths.dist.theme = "./_theme"
+uswds.paths.src.projectSass = "/assets/css"
 
-exports.init  = uswds.init
+exports.init = uswds.init
 exports.compile = uswds.compile
 exports.watch = uswds.watch
